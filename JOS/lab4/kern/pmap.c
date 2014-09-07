22e7f144dd440d6d6f23c2b9b7f34a1efc06e37d
xrandomwalk <xrandomwalk@gmail.com>
2014-09-07 11:17:07 +0900

    lab4 exe 1

--- JOS/lab4/kern/pmap.c
+++ JOS/lab4/kern/pmap.c
@@ -658,7 +658,10 @@ mmio_map_region(physaddr_t pa, size_t size)
 	// Hint: The staff solution uses boot_map_region.
 	//
 	// Your code here:
-	panic("mmio_map_region not implemented");
+  void* ret = (void*)base;
+  boot_map_region(kern_pgdir, base, size, pa, PTE_PCD | PTE_PWT);
+  base += size;
+  return ret;
 }
 
 static uintptr_t user_mem_check_addr;
